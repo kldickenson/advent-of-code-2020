3dfcd76f8fa9b1eb9f0eaad35e2d4f766ff57b25
Karen Dickenson <kldinteractive@gmail.com>
2020-12-06 16:07:31 -0500

    Day 5 completed

--- day05.js
+++ day05.js
@@ -21,7 +21,7 @@ function whichSeat(input) {
 	let seatId = [];
 
 	// loop through the boarding passes
-	for (let k = 0; k< 3; k++){
+	for (let k = 0; k< input.length; k++){
 
 		//change boardingPass string to array
 		passArr = input[k].split("");
@@ -46,12 +46,12 @@ function whichSeat(input) {
 					let firstHalf = [];
 					firstHalf = startRows.slice(0, (startRows.length/2));
 					startRows = firstHalf; // reset Rows
-					console.log(passArr[m] + " -> " + startRows);
+					// console.log(passArr[m] + " -> " + startRows);
 				} else if (passArr[m] === "B") {
 					let secondHalf = [];
 					secondHalf = startRows.slice((startRows.length/2), (startRows.length));
 					startRows = secondHalf; // reset Rows
-					console.log(passArr[m] + " -> " + startRows);
+					// console.log(passArr[m] + " -> " + startRows);
 				}
 				break;
 			case 7:
@@ -62,16 +62,18 @@ function whichSeat(input) {
 					let firstHalf = [];
 					firstHalf = startCols.slice(0, (startCols.length/2));
 					startCols = firstHalf; // reset Cols
-					console.log(passArr[m] + " -> " + startRows + " & " + startCols);
+					// console.log(passArr[m] + " -> " + startRows + " & " + startCols);
 				} else if (passArr[m] === "R") {
 					let secondHalf = [];
 					secondHalf = startCols.slice((startCols.length/2), (startCols.length));
 					startCols = secondHalf; // reset Cols
-					console.log(passArr[m] + " -> " + startRows + " & " + startCols);
+					// console.log(passArr[m] + " -> " + startRows + " & " + startCols);
 				}
 				break;
 			case 10:
 				// calculating seatID
+				console.log(startRows + " & " + startCols);
+				console.log((parseInt(startRows,10) * 8) + parseInt(startCols, 10));
 				seatId.push((parseInt(startRows,10) * 8) + parseInt(startCols, 10));
 				break;
 			}
@@ -79,4 +81,16 @@ function whichSeat(input) {
 	}
 	console.log(seatId);
 	// return the highest seatId
+	console.log(Math.max.apply(null, seatId));
+	let seatsSorted = seatId.sort((a, b) => a - b);
+	console.log(seatsSorted);
+	for (let p = 0; p< seatsSorted.length; p++) {
+		if(seatsSorted[p] != (seatsSorted[p-1] + 1)) {
+			console.log(seatsSorted[p] -1);
+		}
+	}
+	console.log(seatsSorted.includes(609)); // double check
+	console.log(seatsSorted.includes(610)); // double check
+	console.log(seatsSorted.includes(611)); // double check
+
 }
