eed2018da4f4d2f13e229e7b0a9692c34b54744e
Karen Dickenson <kldinteractive@gmail.com>
2020-12-07 15:15:00 -0500

    Day 6 Part 2 completed

--- day06.js
+++ day06.js
@@ -27,18 +27,23 @@ function numQs (input) {
 
 function allYesQs (input) {
 	let fullResults = [];
+	let questions = ["a", "b", "c", "d", "e", "f", "g", "h", "i","j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 	// groups
 	for (let i = 0; i< input.length; i++) {
 		let groupResults = [];
-		console.log(input[i]);
-		// answers
-		for (let j = 0; j < input[i].length; j++) {
-			// convert answers to arrays
-			let ltrs = input[i][j].split("");
-			// loop through each answer (especially for multi-ltr answer)
-			for (let k = 0; k < ltrs.length; k++) {
-				// only collect a ltr that EVERYONE has
+		console.log("Group "+ i + ": " + input[i]);
+
+		// loop through questions
+		for (const ltr of questions) {
+			let count = 0; // reset count of each ltr
+
+			// answers
+			for (let j = 0; j < input[i].length; j++) {
+				input[i][j].includes(ltr) ? count++: null;
+			}
+			if(count === input[i].length) {
+				groupResults.push(ltr);
 			}
 		}
 		console.log("Group Results: " + groupResults);
