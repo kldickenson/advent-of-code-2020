1264336f4fc1681b3bbf2ab78b86f33e76c87420
Karen Dickenson <kldinteractive@gmail.com>
2020-12-09 15:05:50 -0500

    Day 8, working on Part 2, not working yet

--- day08.js
+++ day08.js
@@ -32,12 +32,15 @@ function accumulatorCk(data) {
 	}
 }
 
-function nopJmpCk(data) {
+// Need these variables to be global so they can be accessed in multiple function;
+let nopLines = [];
+let jmpLines = [];
 
+function nopJmpCk(data) {
 	// create an array of visited lines to keep track of where we have been
 	let lines = [];
-	let jmpLines = [];
-	let nopLines = [];
+	// jmpLines = [];
+	// nopLines = [];
 
 	// Need to start at line 1 = data[0]
 	let currentIndex = 0;
@@ -49,12 +52,12 @@ function nopJmpCk(data) {
 			if (lines.includes(currentIndex)) {
 				console.log("This is where it repeats! Line" + currentIndex + ": [" + data[currentIndex] + "]");
 				// console.log("Lines: " + lines);
-				console.log("Accumulator: " + accumulator);
-				console.log("JMP:  (" + jmpLines.length + ")  " + jmpLines);
-				console.log("NOP:  (" + nopLines.length + ")  " + nopLines);
+				// console.log("Accumulator: " + accumulator);
+				// console.log("JMP:  (" + jmpLines.length + ")  " + jmpLines);
+				// console.log("NOP:  (" + nopLines.length + ")  " + nopLines);
 				return "STOP!";
 			} else {
-				console.log("Current -> Line:" + currentIndex + ": " + data[currentIndex]);
+				// console.log("Current -> Line:" + currentIndex + ": " + data[currentIndex]);
 				lines.push(currentIndex);
 				switch(data[currentIndex][0]) {
 				case "acc":
@@ -75,9 +78,37 @@ function nopJmpCk(data) {
 				}
 			}
 		} else {
+			//reset jmpLines and nopLines
+			jmpLines = [];
+			nopLines = [];
 			// had NO infinite loop!
 			console.log("Accumulator: " + accumulator);
 			return "MADE IT TO THE END!";
+
 		}
+	}
+}
+
+function nopLoop(data) {
+	nopJmpCk(data);
+	console.log(nopLines);
+	// loop through the lines that have "nop", change to "jmp", rerun nopJmpCk and check if they make it to the end
+	for (let idx of nopLines) {
+		let dataNew = [...data];
+		dataNew[idx][0] = "jmp";
+		console.log("dataNew  Line:"+ idx + ":  " + dataNew[idx]);
+		nopJmpCk(dataNew);
+	}
+}
+
+function jmpLoop(data) {
+	nopJmpCk(data);
+	console.log(jmpLines);
+	// loop through the lines that have "nop", change to "jmp", rerun nopJmpCk and check if they make it to the end
+	for (let idx of jmpLines) {
+		let dataNew = [...data];
+		dataNew[idx][0] = "nop";
+		console.log("dataNew  Line:"+ idx + ":  " + dataNew[idx]);
+		nopJmpCk(dataNew);
 	}
 }
