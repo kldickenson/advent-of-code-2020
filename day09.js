4be5ead1cd3e73790ab4dc2ea75153910e5bf882
Karen Dickenson <kldinteractive@gmail.com>
2020-12-18 14:22:45 -0500

    Day09, Part 1 working after pairing session with Courtney

--- day09.js
+++ day09.js
@@ -1,58 +1,59 @@
 
 function twoSums(nums, target) {
 	let count = 0; // reset count with each twoSum
+	let uniqueNums = [...new Set(nums)];
+	// hash of pres
 	let numObject = {};
-	for (let i = 0; i < nums.length; i++) {
-		let thisNum = nums[i];
+	for (let i = 0; i < uniqueNums.length; i++) {
+		let thisNum = uniqueNums[i];
 		numObject[thisNum] = i;
 	}
-	for (var i = 0; i < nums.length; i++) {
-		let diff = target - nums[i];
-		if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
-			console.log("Yes, we have a sum!" + nums[i] + " & " + diff + " = " + target);
+	// console.log(numObject);
+	for (var i = 0; i < uniqueNums.length; i++) {
+		if(count > 0 ){
+			return true;
+		}
+		let diff = target - uniqueNums[i];
+		if (numObject.hasOwnProperty(diff)) {
+			// console.log("Yes, we have a sum! " + uniqueNums[i] + " & " + diff + " = " + target);
 			count++;
-			return [i, numObject[diff]];
 		}
 	}
-	count > 0 ? console.log(count): console.log("First ZERO count! The target # was " + target);
-	// return "STOP";
+	return false;
 }
 
 function testLoop(pre, post, fullData) {
-
 	// for each post entry, test to see if it is a sum of two pre numbers
 	//loop through post
 	if (post.length >= 1){
 		for (let i = 0; i<post.length; i++) {
 		// loop through the pres numbers
 			let target = post[i];
-			// console.log(target);
-			twoSums(pre, target);
-			fullData.shift();
-			createPreamble(fullData, pre.length);
+			let result = twoSums(pre, target);
+			if (result === true) {
+				fullData.shift();
+				return(createPreamble(fullData, pre.length));
+			} else {
+				return target;
+			}
 		}
 	}
 }
 
 function createPreamble(fullData, preNum) {
-	console.log("- - - - - - - - - - - - - -");
 	let pre = [];
 	let post = [];
-	console.log("FullData.length = " + fullData.length);
-
 	// splitting the fullData array into the preamble (pre) and the rest (post)
 	if(fullData.length > preNum) {
+		console.log("- - - - - - - - - - - - - -");
+		console.log("FullData.length = " + fullData.length);
 		pre = fullData.slice(0, preNum);
-		console.log("Pre:");
-		console.log(pre);
 		post = fullData.slice(preNum, fullData.length);
-		console.log("Post:");
-		console.log(post);
-		testLoop(pre, post, fullData);
+		return(testLoop(pre, post, fullData));
 	}
 }
 
 function start(input, preNum) {
 	let fullData = [...input];
-	createPreamble(fullData, preNum);
+	return(createPreamble(fullData, preNum));
 }
